bd0e92d357e7da61d1073207004c465c4220c3f6
angonga <andre.gari.2991@gmail.com>
2021-04-16 02:06:39 +0000

    Post Characters and Users Token

--- src/main.py
+++ src/main.py
@@ -8,38 +8,144 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
-
+from models import db, User, Characters
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import JWTManager
+# from models import Person
+# import JWT for tokenization
+from flask_jwt_extended import (
+    JWTManager,
+    jwt_required,
+    get_jwt_identity,
+    create_access_token,
+)
+# Server creation with Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_CONNECTION_STRING")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+jwt = JWTManager(app)
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
-@app.route('/')
+@app.route("/")
 def sitemap():
     return generate_sitemap(app)
-
-@app.route('/user', methods=['GET'])
+@app.route("/user", methods=["GET"])
 def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
+    response_body = {"msg": "Hello, this is your GET /user response "}
     return jsonify(response_body), 200
-
+# register endpoint
+@app.route("/register", methods=["POST"])
+def register_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # validation of possible empty inputs
+    if email is None:
+        return jsonify({"msg": "No email was provided"}), 400
+    if password is None:
+        return jsonify({"msg": "No password was provided"}), 400
+    # busca usuario en BBDD
+    user = User.query.filter_by(email=email).first()
+    if user:
+        # the user was not found on the database
+        return jsonify({"msg": "User already exists"}), 401
+    else:
+        # crea nuevo usuario
+        new_user = User()
+        new_user.email = email
+        new_user.password = password
+        # crea registro nuevo en BBDD de
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"msg": "User created successfully"}), 200
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # valida si estan vacios los ingresos
+    if email is None:
+        return jsonify({"msg": "No email was provided"}), 400
+    if password is None:
+        return jsonify({"msg": "No password was provided"}), 400
+    # para proteger contraseñas usen hashed_password
+    # busca usuario en BBDD
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Invalid username or password"}), 401
+    else:
+        # crear token
+        my_token = create_access_token(identity=user.id)
+        return jsonify({"token": my_token})
+# PORQUE CARAJOS PROTECTED TIENE DOBLE COMILLAS Y EL RESTO, COMILLA SENCILLA
+@app.route("/protected", methods=["GET", "POST"])
+# protege ruta con esta funcion
+@jwt_required()
+def protected():
+    # busca la identidad del token
+    current_id = get_jwt_identity()
+    # busca usuarios en base de datos
+    user = User.query.get(current_id)
+    print(user)
+    return jsonify({"id": user.id, "email": user.email}), 200
+# add characters endpoint
+@app.route("/characters", methods=["POST"])
+def add_characters():
+    name = request.json.get("name", None)
+    birth_year = request.json.get("birth_year", None)
+    gender = request.json.get("gender", None)
+    height = request.json.get("height", None)
+    skin_color = request.json.get("skin_color", None)
+    hair_color = request.json.get("hair_color", None)
+    eye_color = request.json.get("eye_color", None)
+    # validation of possible empty inputs
+    if name is None:
+        return jsonify({"msg": "No name was provided"}), 400
+    if birth_year is None:
+        return jsonify({"msg": "No birth year was provided"}), 400
+    if gender is None:
+        return jsonify({"msg": "No gender was provided"}), 400
+    if height is None:
+        return jsonify({"msg": "No height was provided"}), 400
+    if skin_color is None:
+        return jsonify({"msg": "No skin color was provided"}), 400
+    if hair_color is None:
+        return jsonify({"msg": "No hair color was provided"}), 400
+    if eye_color is None:
+        return jsonify({"msg": "No eye color was provided"}), 400
+    # busca character en BBDD
+    character = Characters.query.filter_by(name=name).first()
+    if character:
+        # the  was found on the database
+        return jsonify({"msg": "Character already exists"}), 401
+    else:
+        new_character = Characters()
+        new_character.name = name
+        new_character.birth_year = birth_year
+        new_character.gender = gender
+        new_character.height = height
+        new_character.skin_color = skin_color
+        new_character.hair_color = hair_color
+        new_character.eye_color = eye_color
+        db.session.add(new_character)
+        db.session.commit()
+        return jsonify({"msg": "Character created successfully"}), 200
+# FAVORITOS (POST, DELETE) = TODOLIST PROJECT
+# CHARACTER (GET)
+# PLANETS (GET)
+# PUEDO CREAR UN ENDPOINT PARA CARGAR INFO A MI BD, PODRIA SER CON CRUD
+# Y QUE LO MANEJE POR APARTE (CRUD PARA PEOPLE. CRUD PARA CHARACTERS)
+# HACER PRIMERO ESTO: CARGAR LOS DATOS A TRAVÉS DE POSTMAN. Y LE HAGO EL POST A MI TABLA ESPECIFICA
+# DE ACUERDO AL TIEMPO CREAMOS OTRO COMPONENTE PARA EL CRUD
 # this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -6,14 +6,26 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            # esto es lo que se va a exponerme por eso no se puede enviar datos sensibles en la serialización
         }
+
+class Characters(db.Model):
+   #__tablename__ = 'characters'
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(250), nullable=False)
+   birth_year = db.Column(db.String(250), nullable=False)
+   gender = db.Column(db.String(250), nullable=False)
+   height = db.Column(db.Integer, nullable=False)
+   skin_color = db.Column(db.String(250), nullable=False)
+   hair_color = db.Column(db.String(250), nullable=False)
+   eye_color = db.Column(db.String(250), nullable=False)
+   #like = relationship(Favorites)
